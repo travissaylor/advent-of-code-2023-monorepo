7c04d4430c0eefaa7933980bf45f415abd9a4d8b
Travis Saylor <travis.saylor@gmail.com>
2023-12-03 13:36:24 -0500

    Add base setup for 02 solutions

--- apps/02/src/index.ts
+++ apps/02/src/index.ts
@@ -1,1 +1,42 @@
-console.log('02')
+import * as fs from "fs"
+
+type CubesRevealed = {
+    blue: number
+    green: number
+    red: number
+}
+
+type GameResult = { index: number; record: CubesRevealed[] }
+
+function parseIndexFromText(rawText: string): number {
+    const indexString = rawText.split("Game ")[1]?.split(":")[0]
+    if (!indexString) {
+        throw new Error("Invalid index")
+    }
+    return parseInt(indexString)
+}
+
+function parseGameResultFromText(rawText: string): GameResult {
+    const cubesRevealed: CubesRevealed[] = []
+    const index = parseIndexFromText(rawText)
+    return { index, record: cubesRevealed }
+}
+
+function getGameResults(rawTextResults: string[]): GameResult[] {
+    return rawTextResults.map(parseGameResultFromText)
+}
+
+function textFileToArray(filename: string) {
+    const text = fs.readFileSync(filename, "utf8")
+    return text.split("\n")
+}
+
+function execute() {
+    const rawTextResults = textFileToArray(
+        "adventofcode.com_2023_day_2_input.txt"
+    )
+    const gameResults = getGameResults(rawTextResults)
+    console.log(gameResults)
+}
+
+execute()
